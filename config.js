7e36454c34f8cda2bca7eb9b748b5dc89531c514
Geoffrey Odari <geoffrey.ochenge@gmail.com>
2022-08-05 21:36:12 +0300

    added a favicon

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-let mongodb_url = 'mongodb://localhost/';
-let dbName = 'darkroom';
+let mongodb_url = 'mongodb+srv://odari:hPhMqq4BzVuSrksZ@cluster0.7wdrl.mongodb.net/?retryWrites=true&w=majority';
+let dbName = 'maendeleo_';
 module.exports ={
 
     mongoDB_URI: process.env.MONGODB_URI || mongodb_url + dbName
